41103a737f7c24c9118e2a3afcfff8c5435474ce
thest <thest@DESKTOP-QOCRTF5>
2021-07-27 15:53:03 -0400

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                string url = "https://0bwg5gwq5b.execute-api.us-east-2.amazonaws.com/DEV/v1/convert";
+
+                var client = new RestClient(url);
+
+                var request = new RestRequest();
+
+                var body = new post { initialValue = 11, initialUOM = "lb", desiredUOM = "kg", result = "" };
+
+                request.AddJsonBody(body);
+
+                var response = client.Post(request);
+
+            textBox1.Text = "(response.StatusCode.ToString()" + ""    + response.Content.ToString();
+                Console.WriteLine(response.StatusCode.ToString() + "   " + response.Content.ToString());
+
+
+                Console.Read();
+            }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+    
+
---
+++ WindowsFormsApp4/post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class post
+    {
+        public int initialValue { get; set; }
+
+        public string initialUOM { get; set; }
+
+        public string desiredUOM { get; set; }
+
+        public string result { get; set; }
+    }
+}
